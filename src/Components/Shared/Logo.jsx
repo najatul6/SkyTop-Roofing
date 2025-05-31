942cf4e8326293c5b6c40a3a9083a8654d14a3b0
najatul6 <mdnajatulislam.develop@gmail.com>
2025-05-31 09:48:55 +0600

    Fix spacing in Logo component for improved visual consistency

--- src/Components/Shared/Logo.jsx
+++ src/Components/Shared/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <div className="text-2xl font-bold text-blue-600 tracking-wide animate__flipInX animate__animated">
-    <span className="text-gray-800">Sky</span>Top<span className="text-red-500">.</span>
+    <span className="text-gray-800">Sky</span>Top<span className="text-red-500">. </span>
   </div>
   )
 }
