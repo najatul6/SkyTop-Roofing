aca03e2709cc25cf4eb90584442f9adf1f0af8b8
najatul6 <mdnajatulislam.develop@gmail.com>
2025-08-08 00:32:51 +0600

    Add footer with copyright and developer information for improved site branding

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -6,6 +6,24 @@ const Layout = () => {
         <div>
             <Navbar/>
             <Outlet />
+            <footer>
+                <div className="bg-neutral text-white p-5 text-center">
+                    <p className="text-sm">
+                        &copy; {new Date().getFullYear()} All Rights Reserved by Dream School
+                    </p>
+                    <p className="text-sm">
+                        Developed By -{" "}
+                        <a
+                            href="https://najatul-islam.vercel.app/"
+                            className="text-danger-color underline capitalize font-bold"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Najatul Islam
+                        </a>
+                    </p>
+                </div>
+            </footer>
         </div>
     )
 }
